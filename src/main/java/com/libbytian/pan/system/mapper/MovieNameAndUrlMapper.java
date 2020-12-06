84bd650eb1800ddb4b09b520a08859014e669be3
sunqi <sun7127@126.com>
2020-12-06 16:50:44 +0800

    补提代码

---
+++ src/main/java/com/libbytian/pan/system/mapper/MovieNameAndUrlMapper.java
@@ -0,0 +1,25 @@
+package com.libbytian.pan.system.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.libbytian.pan.system.model.MovieNameAndUrlModel;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+
+/**
+ * @author: QiSun
+ * @date: 2020-12-06
+ * @Description:
+ */
+@Mapper
+public interface MovieNameAndUrlMapper extends BaseMapper<MovieNameAndUrlModel> {
+
+
+    List<MovieNameAndUrlModel> selectMovieUrlByName (String movieName)   ;
+
+
+     int insertMovieUrls(List<MovieNameAndUrlModel> movieNameAndUrlModels);
+
+     int deleteUrlMovieUrls(MovieNameAndUrlModel movieNameAndUrlModel);
+}
